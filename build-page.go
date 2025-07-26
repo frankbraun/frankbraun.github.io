92a135313032b331e7da84c96eb76ab45a4830a4
Frank Braun <me@frankbraun.org>
2025-07-26 12:00:44 +0200

    fix author

--- build-page.go
+++ build-page.go
@@ -43,7 +43,9 @@ const footer = `</main>
 const atomHeader = `<?xml version="1.0" encoding="UTF-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">
 <title>Frank Braun</title>
-<author>Frank Braun</author>
+<author>
+  <name>Frank Braun</name>
+</author>
 <id>https://frankbraun.org/atom.xml</id>
 <link rel="alternate" type="text/html" href="/"/>
 `
