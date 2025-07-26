7573473b4b8d6c1c2a91d6e300329d3eb4d99bbd
Frank Braun <me@frankbraun.org>
2025-07-26 12:38:57 +0200

    add links to favicons

--- build-page.go
+++ build-page.go
@@ -28,6 +28,10 @@ const twitterCard = `<meta name="twitter:card" content="summary">
 const closeHeader = `<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/css/water.css">
 <link rel="alternate" type="application/atom+xml" title="Frank Braun" href="/atom.xml">
+<link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" type="image/png" sizes="16x16"  href="/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32"  href="/favicon-32x32.png">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
 </head>
 <body>
 <main>
