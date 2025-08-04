1ed34c8aad040dbe8bef3a941ee1b690b5c1faf1
Frank Braun <me@frankbraun.org>
2025-08-05 01:54:55 +0200

    parse markdown and emit HTML

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-fn build_page() {
+use std::fs;
 
+fn build_page() -> Result<(), std::io::Error> {
+    let input = fs::read_to_string("index.md")?;
+    let out = markdown::to_html(&input);
+    print!("{out}");
+    Ok(())
 }
 
 fn main() {
-    build_page();
+    if let Err(e) = build_page() {
+        eprintln!("Error: {e}");
+    }
 }
