194b936904f894b498d6ad323e838d3f14dfe8a1
Frank Braun <me@frankbraun.org>
2025-08-05 01:59:02 +0200

    add DOCTYPE const

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::fs;
 
+const DOCTYPE: &str = r#"<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8"
+"#;
+
 fn build_page() -> Result<(), std::io::Error> {
     let input = fs::read_to_string("index.md")?;
     let out = markdown::to_html(&input);
+    print!("{DOCTYPE}");
     print!("{out}");
     Ok(())
 }
